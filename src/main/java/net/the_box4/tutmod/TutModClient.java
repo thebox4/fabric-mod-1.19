c31d8e26bbaaa90745abc93f10159a9eca53a408
thebox4 <kfir.fibach@gmail.com>
2022-11-30 20:20:22 +0200

    Initial commit

---
+++ src/main/java/net/the_box4/tutmod/TutModClient.java
@@ -0,0 +1,10 @@
+package net.the_box4.tutmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
